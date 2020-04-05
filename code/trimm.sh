374c916282fcf939416e27f0a9ebd7603ac2c38f
csrkang <csrkang@umich.edu>
2020-04-05 15:10:00 -0400

    Trimmomatic and post-trim fastQC scripts

--- code/trimm.sh
+++ code/trimm.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 indir=$1
 outdir=$2
+adapterPath=$3
 for infile in $indir/*_1.fastq.gz
 do
   base=$(basename ${infile} _1.fastq.gz)
   trimmomatic PE -phred33 ${infile} ${indir}/${base}_2.fastq.gz \
                ${outdir}/${base}_1_paired.fastq.gz ${outdir}/${base}_1_unpaired.fastq.gz \
                ${outdir}/${base}_2_paired.fastq.gz ${outdir}/${base}_2_unpaired.fastq.gz \
-               ILLUMINACLIP:${indir}/metagenome_adapters.fasta:2:40:15 \
-               LEADING:3 TRAILING:3 MINLEN:24 SLIDINGWINDOW:4:15
+               ILLUMINACLIP:${adapterPath}/metagenome_adapters.fasta:2:40:15 \
+               LEADING:3 TRAILING:3 MINLEN:24 SLIDINGWINDOW:4:15 \
+  &> ${outdir}/${base}_logX.txt
+  grep -vwE '(Skipping|Using)' ${outdir}/${base}_logX.txt > ${outdir}/${base}_log.txt
+  rm ${outdir}/${base}_logX.txt
 done
