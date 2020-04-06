b719cf46944c763dd154bbd3a3d2618f6186cb14
csrkang <csrkang@umich.edu>
2020-04-06 15:54:23 -0400

    trimmomatic script

--- code/trimm.sh
+++ code/trimm.sh
@@ -5,10 +5,11 @@ adapterPath=$3
 for infile in $indir/*_1.fastq.gz
 do
   base=$(basename ${infile} _1.fastq.gz)
-  trimmomatic PE -phred33 ${infile} ${indir}/${base}_2.fastq.gz \
+  trimmomatic PE -phred33 -thread 4 \
+               ${infile} ${indir}/${base}_2.fastq.gz \
                ${outdir}/${base}_1_paired.fastq.gz ${outdir}/${base}_1_unpaired.fastq.gz \
                ${outdir}/${base}_2_paired.fastq.gz ${outdir}/${base}_2_unpaired.fastq.gz \
-               ILLUMINACLIP:${adapterPath}/metagenome_adapters.fasta:2:40:15 \
+               ILLUMINACLIP:${adapterPath}:2:40:15 \
                LEADING:3 TRAILING:3 MINLEN:24 SLIDINGWINDOW:4:15 \
   &> ${outdir}/${base}_logX.txt
   grep -vwE '(Skipping|Using)' ${outdir}/${base}_logX.txt > ${outdir}/${base}_log.txt
