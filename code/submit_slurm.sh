a67dd78a2f348f0e0310146b5bc9e34d9aefb18a
Kelly Sovacool <kellysovacool@gmail.com>
2020-04-07 11:48:34 -0400

    Remove my email

--- code/submit_slurm.sh
+++ code/submit_slurm.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --output=log/slurm-%j_%x.out
 #SBATCH --account=class
-#SBATCH --mail-user=sovacool@umich.edu
+#SBATCH --mail-user=YOUR_EMAIL
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 time snakemake -n
