fa8d580438c9b237ee6269801f1a6222335d2c56
Brittany Brown Hicks <britbrow@kickseed.dcmb.ybinternal>
2020-04-11 21:20:10 -0400

    Changed to use fasterq-dump

--- code/download.sh
+++ code/download.sh
@@ -4,5 +4,6 @@ outdir=$2
 for SRA in $(cat $list_file)
 do
     prefetch $SRA
-    fasterq-dump --split-files $SRA -O $outdir --gzip
+    fasterq-dump --split-files $SRA -O $outdir #--gzip 
+#Brittany: I commented out --gzip because fasterq doesn't recognize gzip, only works for fastq
 done
