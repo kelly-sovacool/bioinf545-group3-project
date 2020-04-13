53af62eae40a69cd9904864f447fbd312a02a6f9
csrkang <csrkang@umich.edu>
2020-04-12 20:22:32 -0400

    Changed output naming scheme for downstream processes

--- code/trimm.sh
+++ code/trimm.sh
@@ -7,8 +7,8 @@ do
   base=$(basename ${infile} _1.fastq.gz)
   trimmomatic PE -phred33 -threads 4 \
                ${infile} ${indir}/${base}_2.fastq.gz \
-               ${outdir}/${base}_1_paired.fastq.gz ${outdir}/${base}_1_unpaired.fastq.gz \
-               ${outdir}/${base}_2_paired.fastq.gz ${outdir}/${base}_2_unpaired.fastq.gz \
+               ${outdir}/${base}_paired_1.fastq.gz ${outdir}/${base}_unpaired_1.fastq.gz \
+               ${outdir}/${base}_paired_2.fastq.gz ${outdir}/${base}_unpaired_2.fastq.gz \
                ILLUMINACLIP:${adapterPath}:2:40:15 \
                LEADING:3 TRAILING:3 MINLEN:24 SLIDINGWINDOW:4:15 \
   &> ${outdir}/${base}_logX.txt
