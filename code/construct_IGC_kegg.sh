b070c752de075fe3650562dc0d578fd3e9e2f643
csrkang <csrkang@umich.edu>
2020-04-13 12:40:30 -0400

    Moved scripts to metagenome folder.

--- code/construct_IGC_kegg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cut -d$'\t' -f1,8 data/metagenome/bwa_DB/IGC.annotation_OF.summary > data/metagenome/bwa_DB/IGC.kegg
-O
--- code/post_fastQC.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-indir=$1
-outdir=$2
-for infile in $indir/*_paired.fastq.gz
-do
-  base=$(basename ${infile} _paired.fastq.gz)
-  fastqc -o ${outdir} ${infile}
-done
