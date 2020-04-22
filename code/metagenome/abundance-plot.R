fa73b35236ad16858aac91bbe08ad4159e5bd71e
Christina Kang-Yun <csrkang@umich.edu>
2020-04-22 04:52:53 -0400

    updated code used to produce figure and tables.

--- code/metagenome/abundance-plot.R
+++ code/metagenome/abundance-plot.R
@@ -6,13 +6,19 @@ library("tidyr")
 library("ggplot2")
 # library("vegan")
 
-
 # Import metadata to order samples by name.
 SraRun <- read.table(here::here("data", "SraRunTable.txt"),
   header = T,
   sep = ",", stringsAsFactors = F
 ) %>%
   filter(samp_mat_process == "WholeMetagenome") %>% # Filter out virome samples
+  filter(Run != 'SRR5665024' & # remove negative control and samples with extremely low read count
+           Run != 'SRR5665060' &
+           Run != 'SRR5665081' &
+           Run != 'SRR5665121' &
+           Run != 'SRR5665138' &
+           Run != 'SRR5665117' &
+           Run != 'SRR5665160') %>%
   arrange(subjectid) %>% # rearrange by patient type
   arrange(factor(DiseaseClass,
     levels = c("Negative", "Healthy", "Adenoma", "Cancer")
@@ -36,7 +42,7 @@ colnames(keggCounts) <-
   c("KeggNo", gsub(".*(SRR.*)_keggCount.txt", "\\1", fileNames)) # rename columns to sample name.
 
 keggCounts <- keggCounts %>%
-  select(paste(colOrder)) # reorder by sample type.
+  select(KeggNo, paste(colOrder)) # reorder by sample type.
 
 colnames(keggCounts) <- c("KeggNo", SraRun$subjectid) # keep KeggNo & add the subject id's
 keggCounts[is.na(keggCounts)] <- 0 # replace NA with 0
@@ -78,16 +84,32 @@ cds <- estimateCommonDisp(cds)
 cds <- estimateTagwiseDisp(cds, prior.df = 10)
 
 # Draw the MDS plot
-plotMDS(cds, main = "MDS Plot for Count Data", labels = colnames(cds$counts), col = groupColors)
+pdf(file = here::here("figures", "gene_abundance_MDS.pdf"), width = 5, height = 5)
+plotMDS(cds, labels = colnames(cds$counts), col = groupColors)
+dev.off()
 
-#### Did not update following code >>> Need to do pairwise?
+# Find differentially abundant genes in healthy vs cancer/adenoma
+DEgenes_HA <- exactTest(cds, pair = c("H", "A"))
+summary(decideTestsDGE(DEgenes_HA, p.value = 0.05))
+DEgene_table_HA <- topTags(DEgenes_HA, n = nrow(DEgenes_HA$table), sort.by = "logFC")$table
+DEgene_table_HA$KeggNo <- rownames(DEgene_table_HA)
+DEgene_table_HA <- DEgene_table_HA %>% filter(PValue <= 0.05)
 
+DEgenes_HC <- exactTest(cds, pair = c("H", "C"))
+summary(decideTestsDGE(DEgenes_HC, p.value = 0.05))
+DEgene_table_HC <- topTags(DEgenes_HC, n = nrow(DEgenes_HC$table), sort.by = "logFC")$table
+DEgene_table_HC$KeggNo <- rownames(DEgene_table_HC)
+DEgene_table_HC <- DEgene_table_HC %>% filter(PValue <= 0.05)
 
-# Find Differentially Expressed genes healthy and control
-DEgenes.HC <- exactTest(cds, pair = c("H", "C"))
-summary(decideTestsDGE(DEgenes.HC, p.value = 0.05))
-DEgene.table.HC <- topTags(DEgenes.HC, n = nrow(DEgenes.HC$table))$table
-write.table(DEgene.table.HC,
-  file = here::here("data", "metagenome", "DEgenes.csv"),
-  sep = ",", row.names = TRUE
-)
+DEgenes <- inner_join(DEgene_table_HA, DEgene_table_HC, by = "KeggNo", suffix = c("_HA", "_HC"))
+
+DEgenes_pos <- DEgenes %>% filter(logFC_HA > 1 & logFC_HC > 1) %>% select(KeggNo) %>% head(500)
+DEgenes_neg <- DEgenes %>% filter(logFC_HA < -1 & logFC_HC < -1) %>% select(KeggNo) %>% head(500)
+
+write.table(DEgenes_pos,
+            file = here::here('data', 'metagenome', 'gene_abundance_results', 'DEgenes_pos.txt'),
+            sep = '\t', row.names = T, quote = F, col.names = F)
+
+write.table(DEgenes_neg,
+            file = here::here('data', 'metagenome', 'gene_abundance_results', 'DEgenes_neg.txt'),
+            sep = '\t', row.names = T, quote = F, col.names = F)
