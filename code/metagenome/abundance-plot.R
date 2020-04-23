bc6f2d64b0fbaf8a1a97270adde202b556a6dc5a
Christina Kang-Yun <csrkang@umich.edu>
2020-04-23 12:20:37 -0400

    MDS plot with new colors and symbol.

--- code/metagenome/abundance-plot.R
+++ code/metagenome/abundance-plot.R
@@ -3,23 +3,25 @@ library("edgeR")
 library("dplyr")
 library("purrr")
 library("tidyr")
+library("ggplot2")
+# library("vegan")
 
 # Import metadata to order samples by name.
 SraRun <- read.table(here::here("data", "SraRunTable.txt"),
-  header = T,
-  sep = ",", stringsAsFactors = F
+                     header = T,
+                     sep = ",", stringsAsFactors = F
 ) %>%
   filter(samp_mat_process == "WholeMetagenome") %>% # Filter out virome samples
   filter(Run != "SRR5665024" & # remove negative control and samples with extremely low read count
-    Run != "SRR5665060" &
-    Run != "SRR5665081" &
-    Run != "SRR5665121" &
-    Run != "SRR5665138" &
-    Run != "SRR5665117" &
-    Run != "SRR5665160") %>%
+           Run != "SRR5665060" &
+           Run != "SRR5665081" &
+           Run != "SRR5665121" &
+           Run != "SRR5665138" &
+           Run != "SRR5665117" &
+           Run != "SRR5665160") %>%
   arrange(subjectid) %>% # rearrange by patient type
   arrange(factor(DiseaseClass,
-    levels = c("Negative", "Healthy", "Adenoma", "Cancer")
+                 levels = c("Negative", "Healthy", "Adenoma", "Cancer")
   ))
 
 colOrder <- as.character(SraRun$Run) # used to order the dataframe of sample keggCounts.
@@ -34,14 +36,19 @@ keggCounts <- lapply(fileNames, function(i) {
   read.csv(i, header = F, stringsAsFactors = F)
 })
 
-keggCounts <- keggCounts %>% reduce(full_join, by = "V1") # full join on kegg ID.
+keggCounts <- keggCounts %>% reduce(full_join, by = "V1") # full join on KEGG No.
 
 colnames(keggCounts) <-
   c("KeggNo", gsub(".*(SRR.*)_keggCount.txt", "\\1", fileNames)) # rename columns to sample name.
 
-keggCounts <- keggCounts %>%
-  select(KeggNo, paste(colOrder)) # reorder by sample type.
+keggCounts <- keggCounts %>% filter(KeggNo != "unknown")
 
+rownames(keggCounts) <- keggCounts$KeggNo
+
+keggCounts <- keggCounts %>%
+  select(paste(colOrder)) # reorder by sample type.
+
+colnames(keggCounts) <- c(SraRun$subjectid) # add the subject ids
 keggCounts[is.na(keggCounts)] <- 0 # replace NA with 0
 
 write.table(keggCounts, file = here::here("data", "metagenome", "all_kegg_counts.csv"), sep = ",", row.names = TRUE)
@@ -56,8 +63,17 @@ group <- c(
   rep("A", sum(SraRun$DiseaseClass == "Adenoma")),
   rep("C", sum(SraRun$DiseaseClass == "Cancer"))
 )
-cds <- DGEList(keggCounts, group = group)
 
+groupColors <-
+  c(
+    rep("black", sum(SraRun$DiseaseClass == "Negative")),
+    rep("#66C2A5", sum(SraRun$DiseaseClass == "Healthy")),
+    rep("#FC8D62", sum(SraRun$DiseaseClass == "Adenoma")),
+    rep("#8DA0CB", sum(SraRun$DiseaseClass == "Cancer"))
+  )
+
+cds <- keggCounts %>%
+  DGEList(group = group) # analyze just the count numbers
 # Filter out genes with low counts, keeping those rows where the count
 # per million (cpm) is at least 1 in at least 6 samples:
 keep <- rowSums(cpm(cds) > 1) >= 6
@@ -70,7 +86,8 @@ cds <- estimateTagwiseDisp(cds, prior.df = 10)
 
 # Draw the MDS plot
 pdf(file = here::here("figures", "gene_abundance_MDS.pdf"), width = 5, height = 5)
-plotMDS(cds, labels = colnames(cds$counts), col = groupColors)
+plotMDS(cds, pch = 19, col = groupColors)
+legend("bottomright",fill=c("#66C2A5","#FC8D62", "#8DA0CB"),legend=unique(factor(SraRun$DiseaseClass)))
 dev.off()
 
 # Find differentially abundant genes in healthy vs cancer/adenoma
