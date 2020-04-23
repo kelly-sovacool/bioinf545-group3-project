30dcfcfffa9bff36053c596b3223d56fa3d95cd3
Christina Kang-Yun <csrkang@umich.edu>
2020-04-22 20:50:20 -0400

    Capitalized names of result files.

--- code/metagenome/mtphln_heatmap.R
+++ code/metagenome/mtphln_heatmap.R
@@ -1,10 +1,6 @@
-install.packages("ggplot2")
-install.packages("dplyr")
-install.packages("tidyr")
 
-library(ggplot2)
-library(dplyr)
-library(tidyr)
+library(tidyverse)
+library(here)
 
 mapSampleToColor <- function(annotations) {
   # Assign color in heatmap.2() based on disease class
@@ -13,53 +9,49 @@ mapSampleToColor <- function(annotations) {
   # Adenoma = blue
   # Cancer = red
   colorsVector <- ifelse(annotations["DiseaseClass"] == "Healthy",
-    "green", ifelse(annotations["DiseaseClass"] == "Adenoma",
-      "blue", "red"
-    )
-  )
+                         "green", ifelse(annotations["DiseaseClass"] == "Adenoma",
+                                         "blue", "red"
+                                         ))
   return(colorsVector)
 }
 
-setwd("/data/metagenome/metaphlan2_results")
-
 # Metadata with sample information
-SraRun <- read.table("../../SraRunTable.txt", header = T, sep = ",", stringsAsFactors = F)
-SraRun <- SraRun %>%
+metadata <- read.table(here("data", "SraRunTable.txt"), header = T, sep = ",", stringsAsFactors = F) %>%
   filter(samp_mat_process == "WholeMetagenome") %>% # Filter out virome samples
-  arrange(subjectid) %>% # rearrange by patient type
-  arrange(factor(DiseaseClass, levels = c("Negative", "Healthy", "Adenoma", "Cancer")))
+  arrange(factor(DiseaseClass, levels = c("Negative", "Healthy", "Adenoma", "Cancer"))) %>%
+  select(subjectid, Run, DiseaseClass) %>%
+  filter(Run != "SRR5665024" & # remove negative control and samples with extremely low read count
+           Run != "SRR5665060" &
+           Run != "SRR5665081" &
+           Run != "SRR5665121" &
+           Run != "SRR5665138" &
+           Run != "SRR5665117" &
+           Run != "SRR5665160")
+metadata$subjectid <- factor(metadata$subjectid)
 
-# Abundance (%) data (could be any level, species/genus/etc., switch out 'merged_species.txt')
-taxAbundance <- read.table("merged_species.txt", header = T, sep = "\t", stringsAsFactors = F)
-rownames(taxAbundance) <- taxAbundance$clade_name # Have organism name as row name
-taxAbundance <- taxAbundance %>%
-  select(-NCBI_tax_id, -clade_name) # Collect abundance values
-colnames(taxAbundance) <- gsub("_mtphln2", "", colnames(taxAbundance)) # Truncate sample names
+# Heat map construction
+plot_heatmap <- function(taxonlevel){
+  taxheatmap <- read.table(here("data", "metagenome","metaphlan2_results",
+                                paste0("merged_",taxonlevel,".txt")), header = T, sep = "\t", stringsAsFactors = F) %>%
+    pivot_longer(cols = starts_with("SRR"), names_to = "Run", values_to = "abun") %>%
+    mutate(Run = gsub("_mtphln2", "", Run))  %>%
+    inner_join(metadata) %>%
+    ggplot(aes(x = subjectid, y = clade_name, fill = abun)) +
+    geom_tile() +
+    scale_fill_distiller(type = "div", na.value = "white", direction = -1, palette = "RdYlBu") +
+    theme_classic() +
+    labs(x = "Subject ID", y = taxonlevel, fill = "Abundance (%)") +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 5),
+          axis.line = element_blank(),
+          axis.ticks = element_blank())
+}
+plotlist<- lapply(c("Phylum", "Family", "Genus", "Species"), plot_heatmap)
 
-# Reorder columns of abundance data based on order in meta data
-colOrder <- as.data.frame(SraRun$Run)
-msCols <- as.data.frame(colnames(taxAbundance))
-colnames(colOrder) <- "Run"
-colnames(msCols) <- "Run"
-colOrder <- inner_join(colOrder, msCols, by = "Run")
-rm(msCols)
-taxAbundance <- taxAbundance %>% select(paste(colOrder$Run)) # Reordering
-taxAbundance <- as.matrix(taxAbundance) # Need matrix for heatmap.2()
+ggsave(filename = here("figures","metag_taxa_heatmap_phylum.png"),
+       width = 10, height = 7,  plot = plotlist[[1]])
 
-# Assign sample colors to meta data
-sampleColors <- mapSampleToColor(SraRun)
-SraRun$sampleColors <- sampleColors
-SraRunCol <- SraRun
-SraRunCol <- inner_join(colOrder, SraRunCol, by = "Run")
-sampleColors <- SraRunCol$sampleColors
-rm(colOrder)
-rm(SraRunCol)
+ggsave(filename = here("figures","metag_taxa_heatmap_family.png"),
+       width = 10, height = 7,  plot = plotlist[[2]])
 
-# Construct heatmap
-heatmap.2(taxAbundance,
-  ColSideColors = sampleColors,
-  trace = "none",
-  scale = "row",
-  col = brewer.pal(11, "RdBu"),
-  dendrogram = "col"
-)
+ggsave(filename = here("figures","metag_taxa_heatmap_genus.png"),
+       width = 10, height = 7,  plot = plotlist[[3]])
